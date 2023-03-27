c683e7346995458e4dab95daa44e773ecdbe7288
yannick <yannick.battail@raisepartner.com>
2023-03-27 17:18:13 +0200

    feat: generate all the STL for the supports for a 1 square meter oven and put them in support_squareMeter_stl.zip

--- CD_solar_oven/generate_all.sh
+++ CD_solar_oven/generate_all.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-time ../openscad_batch/generate_profile.sh -g jpg -p all_support_squareMeter ./CD_solar_oven.scad
-time ../openscad_batch/generate_profile.sh -g jpg -p all_debug_squareMeter ./CD_solar_oven.scad
-time ../openscad_batch/generate_profile.sh -g jpg -p base ./CD_solar_oven.scad
-time ../openscad_batch/generate_profile.sh -g jpg -p support_squareMeter_A1 ./CD_solar_oven.scad
+
+rm -Rf ./CD_solar_oven/ support_squareMeter_stl.zip
 
 for i in {A..E}
 do
@@ -12,3 +10,17 @@ do
          ../openscad_batch/generate_profile.sh -g stl -p support_squareMeter_${i}${j} ./CD_solar_oven.scad
     done
 done
+
+../openscad_batch/generate_profile.sh -g stl -p base ./CD_solar_oven.scad
+
+../openscad_batch/generate_profile.sh -g jpg -p all_support_squareMeter ./CD_solar_oven.scad
+../openscad_batch/generate_profile.sh -g jpg -p all_debug_squareMeter ./CD_solar_oven.scad
+../openscad_batch/generate_profile.sh -g jpg -p base ./CD_solar_oven.scad
+../openscad_batch/generate_profile.sh -g jpg -p support_squareMeter_A1 ./CD_solar_oven.scad
+../openscad_batch/generate_profile.sh -g jpg -p stl ./CD_solar_oven.scad
+
+zip -r support_squareMeter_stl.zip ./CD_solar_oven/
+
+rm ./CD_solar_oven/3D/support_squareMeter_*
+
+../openscad_batch/generate_profile.sh -g stl -p support_squareMeter_A1 ./CD_solar_oven.scad

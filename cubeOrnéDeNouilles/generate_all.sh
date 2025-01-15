7d5cd080dac8af3665e597b0fb1962b9767446d9
Yannick Battail <yannick.battail@gmail.com>
2025-01-15 21:35:04 +0100

    feat(cubeOrnéDeNouilles): generate anim model etc...

---
+++ cubeOrnéDeNouilles/generate_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+run_in_docker() {
+   docker run -it -v ./:/openscad openscad-nightly-tools $@
+}
+
+if [[ $1 == "thingiverse" ]]
+then
+  echo "generate for thingiverse"
+  run_in_docker generate_for_thingiverse.sh cubeOrnéDeNouilles
+else
+  run_in_docker generate_profile.sh -g jpg,webp,3mf ./cubeOrnéDeNouilles.scad
+fi

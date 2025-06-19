98e26a10fe7209088056098bdafd748d6e26577f
Yannick Battail <yannick.battail.external@salomon.com>
2025-06-19 14:32:48 +0200

    fix(jeu_fate): change config file

--- jeu_fate/generate_all.sh
+++ jeu_fate/generate_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-npx openscad-generate generate --outFormats png,webp,3mf --mosaicFormat 2,2 -j 15 --configFile fate.config.json ./fate.scad
-npx openscad-generate generate --outFormats png,webp,3mf --mosaicFormat 4,3 -j 15 --configFile set_de_jeu.config.json ./set_de_jeu.scad
+npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 2,2 -j 15 --configFile fate.json5 ./fate.scad
+npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 4,3 -j 15 --configFile set_de_jeu.json5 ./set_de_jeu.scad

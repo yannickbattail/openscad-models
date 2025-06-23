454a071bd8809662c28894fda9da3d8d986e92c6
Yannick Battail <yannick.battail.external@salomon.com>
2025-06-23 16:22:55 +0200

    feat(jeu_fate): ajout de la piece corner et de l'icone des deplacements

--- jeu_fate/generate_all.sh
+++ jeu_fate/generate_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 2,2 -j 15 --configFile fate.json5 ./fate.scad
+npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 2,3 -j 15 --configFile fate.json5 ./fate.scad
 npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 4,3 -j 15 --configFile set_de_jeu.json5 ./set_de_jeu.scad

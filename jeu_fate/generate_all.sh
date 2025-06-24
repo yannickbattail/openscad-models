ea930e0201ea5142d71d130b8d3716c09011195c
Yannick Battail <yannick.battail.external@salomon.com>
2025-06-24 13:42:17 +0200

    feat(jeu_fate): add 4 icons of element on the edge

--- jeu_fate/generate_all.sh
+++ jeu_fate/generate_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 4,5 -j 15 --configFile fate.json5 ./fate.scad
+npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 5,4 -j 15 --configFile fate.json5 ./fate.scad
 npx openscad-generate@latest generate --outFormats png,webp,3mf --mosaicFormat 4,3 -j 15 --configFile set_de_jeu.json5 ./set_de_jeu.scad
 

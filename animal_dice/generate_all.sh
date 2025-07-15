1357579df374b5fd050b7d249de96c9b3259f020
Yannick Battail <yannick.battail@gmail.com>
2025-07-15 12:23:08 +0200

    feat(animal_dice): add parts

--- animal_dice/generate_all.sh
+++ animal_dice/generate_all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-npx openscad-generate@latest generate -j 14 --outFormats png,webp,3mf --mosaicFormat 4,3 --configFile dice_animal.yaml ./dice_animal.scad
+parallel_jobs=10
+
+npx openscad-generate@latest generate -j $parallel_jobs --outFormats png,webp,3mf --mosaicFormat 6,6 --configFile dice_animal.yaml ./dice_animal.scad

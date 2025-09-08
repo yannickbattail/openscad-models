19b52b9d181e2523e87b6d640b36f3523676f0dd
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:43:52 +0200

    feat(estourballe): duplicate hyperEstourballe to maistreEstourballe

--- estourballe/generate_all.sh
+++ estourballe/generate_all.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-time ../openscad_batch/generate_profile.sh -g jpg,webp,3mf ./estourballe.scad
-time ../openscad_batch/generate_profile.sh -g jpg,webp,3mf ./superEstourballe.scad
-time ../openscad_batch/generate_profile.sh -g jpg,webp,3mf ./hyperEstourballe.scad
+run_in_docker() {
+   docker run -it -v ./:/openscad openscad-nightly-tools $@
+}
+
+if [[ $1 == "thingiverse" ]]
+then
+  echo "generate for thingiverse"
+  run_in_docker generate_for_thingiverse.sh estourballe
+  run_in_docker generate_for_thingiverse.sh superEstourballe
+  run_in_docker generate_for_thingiverse.sh hyperEstourballe
+  run_in_docker generate_for_thingiverse.sh maistreEstourballe
+else
+  run_in_docker generate_profile.sh -g jpg,webp,3mf ./estourballe.scad
+  run_in_docker generate_profile.sh -g jpg,webp,3mf ./superEstourballe.scad
+  run_in_docker generate_profile.sh -g jpg,webp,3mf ./hyperEstourballe.scad
+  run_in_docker generate_profile.sh -g jpg,webp,3mf ./maistreEstourballe.scad
+fi

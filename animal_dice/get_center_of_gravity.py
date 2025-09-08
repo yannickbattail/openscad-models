ce133d086a0d2dbb30bf976e5b16618b5e510762
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:44:02 +0200

    feat(animal_dice): calc all center of gravity

--- animal_dice/get_center_of_gravity.py
+++ animal_dice/get_center_of_gravity.py
@@ -1,10 +1,50 @@
-import sys
+import json
+import os
 
 from stl import mesh
 
-# Using an existing closed stl file:
-your_mesh = mesh.Mesh.from_file(sys.argv[1])
+# Directory containing the .stl files
+folder = "animals/"
+output_file = "animals_center_of_gravity.json"
+preset_file = "dice_animal.json"
 
-volume, cog, inertia = your_mesh.get_mass_properties()
-print("Volume                            = {0}".format(volume))
-print("Position of the center of gravity = {0}".format(cog))
+# Dictionary to store the results
+results = []
+parameterSet = {
+ "fileFormatVersion": "1",
+ "parameterSets": {}
+}
+parts = ""
+
+i = 0
+# Iterate over all .stl files in the folder
+for file in sorted(os.listdir(folder)):
+    if file.endswith(".stl"):
+        print(f"Processing {file}...")
+        file_path = os.path.join(folder, file)
+        your_mesh = mesh.Mesh.from_file(file_path)
+        fileWithoutExt = file.replace(".stl", "")
+
+        _, cog, _ = your_mesh.get_mass_properties()
+        results.append({
+            "filename": file_path,  # filename
+            "center_of_gravity": cog.tolist(),  # center of gravity
+            "scale": 15,  # scale percent
+            "rotation": [0, 0, 0]  # rotation
+        })
+        parameterSet["parameterSets"][fileWithoutExt] = {
+            "part": str(i),
+        }
+        parts += str(i) + ":" + fileWithoutExt + ", "
+        i += 1
+
+# Write the results to a JSON file
+with open(output_file, "w") as f:
+    json.dump(results, f, indent=2)
+
+# Write the results to a JSON file
+with open(preset_file, "w") as f:
+    json.dump(parameterSet, f, indent=2)
+
+print("add to you file:")
+print("part = 0; // [" + parts + "]")

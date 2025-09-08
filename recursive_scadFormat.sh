ce52dd7a59825822d3f969ba85a61207afcd0588
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 14:15:03 +0200

    refactor: recursive_scadFormat.sh

--- recursive_scadFormat.sh
+++ recursive_scadFormat.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 directory=$1
-if [[ -z "$directory"]]
-then
+
+if [ -z "$directory" ]; then
   directory="."
+fi
+
+if [ ! -d "$directory" ]; then
+  echo "Error: '$directory' is not a valid directory."
+  exit 1
 fi
 
 find $directory -type f  -name "*.scad" -exec scadformat "{}" \;

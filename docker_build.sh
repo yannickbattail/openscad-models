8ecdb645aa7a2dcfbc80f8a2cfe704129e7042fc
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:43:55 +0200

    feat(reindeer): update openscad image and f3d to v3.0

--- docker_build.sh
+++ docker_build.sh
@@ -5,6 +5,6 @@ DEBUG=" --progress=plain "
 #DEBUG=" --progress=plain --no-cache "
 
 docker build \
-  --build-arg "F3D_URL=https://github.com/f3d-app/f3d/releases/download/v2.5.0/F3D-2.5.0-Linux-x86_64-headless.deb" \
+  --build-arg "F3D_URL=https://github.com/f3d-app/f3d/releases/download/v3.0.0/F3D-3.0.0-Linux-x86_64.deb" \
   --build-arg "SCADFORMAT_URL=https://github.com/hugheaves/scadformat/releases/download/v0.7/linux.zip" \
   $DEBUG -t openscad-nightly-tools .

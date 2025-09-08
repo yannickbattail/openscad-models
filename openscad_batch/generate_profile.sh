ec3775839b950a6e36c92869bd5c0c7e52072551
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:43:47 +0200

    fix(selfie_saber): fix phone size

--- openscad_batch/generate_profile.sh
+++ openscad_batch/generate_profile.sh
@@ -7,16 +7,17 @@
 # https://github.com/yannickbattail/openscad-models/tree/main/openscad_batch
 ################################################################################
 
-# colors
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-IReset='\e[0m'            # Reset colors
+# Background
+On_Black='\033[40m' On_Red='\033[41m' On_Green='\033[42m' On_Yellow='\033[43m' On_Blue='\033[44m' On_Purple='\033[45m' On_Cyan='\033[46m' On_White='\033[47m' 
+# High Intensity
+IBlack='\033[0;90m' IRed='\033[0;91m' IGreen='\033[0;92m' IYellow='\033[0;93m' IBlue='\033[0;94m' IPurple='\033[0;95m' ICyan='\033[0;96m' IWhite='\033[0;97m' 
+# Bold High Intensity
+BIBlack='\033[1;90m' BIRed='\033[1;91m' BIGreen='\033[1;92m' BIYellow='\033[1;93m' BIBlue='\033[1;94m' BIPurple='\033[1;95m' BICyan='\033[1;96m' BIWhite='\033[1;97m' 
+# High Intensity backgrounds
+On_IBlack='\033[0;100m' On_IRed='\033[0;101m' On_IGreen='\033[0;102m' On_IYellow='\033[0;103m' On_IBlue='\033[0;104m' On_IPurple='\033[0;105m' On_ICyan='\033[0;106m' On_IWhite='\033[0;107m' 
+On_Reset='\e[0m'
+#BRIGHT=$(tput bold) NORMAL=$(tput sgr0) BLINK=$(tput blink) REVERSE=$(tput smso) UNDERLINE=$(tput smul)
+
 
 POSITIONAL_ARGS=()
 

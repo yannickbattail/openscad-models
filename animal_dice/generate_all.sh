664c6c492ea87b9c6958fefbabb84e570078fba4
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:43:58 +0200

    feat(animal_d20): generate_all.sh for all .scad

--- animal_dice/generate_all.sh
+++ animal_dice/generate_all.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 run_in_docker() {
-  docker run -it -v ./:/work openscad-tools $@
+   docker run -it \
+     --user "$(id -u):$(id -g)" \
+     -v ./:/openscad \
+     openscad-nightly-tools $@
 }
 
-if [[ $1 == "thingiverse" ]]
-then
-  echo "generate for thingiverse"
-  run_in_docker generate_for_thingiverse.sh dice_animal
-else
-  run_in_docker generate_profile.sh -g jpg,webp,3mf ./dice_animal.scad
-fi
+for file in *.scad; do
+  if [[ $1 == "thingiverse" ]]
+  then
+    echo "generate for thingiverse"
+    run_in_docker generate_for_thingiverse.sh "$(basename ${file} .scad)"
+  else
+    run_in_docker generate_profile.sh -g jpg,webp,3mf "${file}"
+  fi
+done

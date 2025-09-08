55285d3c3d39176aa823a19df713b2ae30a695a5
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:43:05 +0200

    generate_profile.sh add colors

--- pipe/generate_all.sh
+++ pipe/generate_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-time ./generate_profile.sh --config-file pipe2_examples-config.sh ./pipe1_examples.scad
-time ./generate_profile.sh --config-file pipe2_examples-config.sh ./pipe2_examples.scad
+time ./generate_profile.sh ./pipe1_examples.scad
+time ./generate_profile.sh ./pipe2_examples.scad
 ### example of full usage
-# ./generate_profile.sh --config-file pipe2_examples-config.sh --only-parameter-set example --generate stl ./pipe2_examples.scad
+# ./generate_profile.sh --config-file pipe2_examples.conf --only-parameter-set example --generate stl ./pipe2_examples.scad
 ### same but with short options
-# ./generate_profile.sh -c pipe2_examples-config.sh -p example -g stl ./pipe2_examples.scad
+# ./generate_profile.sh -c pipe2_examples.conf.sh -p example -g stl ./pipe2_examples.scad

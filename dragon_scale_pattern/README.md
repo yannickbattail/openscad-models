66ecac2d28394f5df13967fe8521198bfe709665
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:43:22 +0200

    feat: dragon scales in different triangle patterns

---
+++ dragon_scale_pattern/README.md
@@ -0,0 +1,7 @@
+# dragon_scale_pattern
+
+scale is from https://www.thingiverse.com/thing:3470986
+
+### TODO
+
+- Enter a custom matrix as string
--- dragon_scale_pattern/generate_all.sh
+++ dragon_scale_pattern/generate_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -Rf ./dragon_scale_pattern && time ../openscad_batch/generate_profile.sh ./dragon_scale_pattern.scad
+time ../openscad_batch/generate_profile.sh -g jpg,webp,3mf ./dragon_scale_pattern.scad

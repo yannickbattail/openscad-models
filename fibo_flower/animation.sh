86ded574d60a485637723e0bd01f9ee5bb90abb4
Yannick Battail <yannick.battail.external@salomon.com>
2025-09-08 16:42:52 +0200

    animation for all flowers

--- fibo_flower/animation.sh
+++ fibo_flower/animation.sh
@@ -18,11 +18,40 @@ img_size=521,512
 form="petal" # cone or petal
 
 mkdir ./anim
-echo rendering images ...
+echo rendering images ${form} ...
 openscad -q -o ./anim/${scad_file}.png -D "form=\"${form}\"" -D "animation_rotation=true" --animate ${nb_image} --imgsize ${img_size} ./${scad_file}.scad
 
-echo generating animation ...
+echo generating animation ${form} ...
 convert -delay ${delay} -loop 0 ./anim/${scad_file}*.png ./${scad_file}.gif
 
-echo cleanup animation ...
+echo cleanup animation ${form} ...
 rm -Rf ./anim/
+mv ./${scad_file}.gif ./${scad_file}_petal.gif
+
+#### fibo_flower config ###
+form="cone" # cone or petal
+
+mkdir ./anim
+echo rendering images wriggle ${form} ...
+openscad -q -o ./anim/${scad_file}.png -D "form=\"${form}\"" -D "animation_rotation=true" --animate ${nb_image} --imgsize ${img_size} ./${scad_file}.scad
+
+echo generating animation wriggle ${form} ...
+convert -delay ${delay} -loop 0 ./anim/${scad_file}*.png ./${scad_file}.gif
+
+echo cleanup animation wriggle ${form} ...
+rm -Rf ./anim/
+mv ./${scad_file}.gif ./${scad_file}_wriggle_cone.gif
+
+#### fibo_flower config ###
+form="cone" # cone or petal
+
+mkdir ./anim
+echo rendering images fixed ${form} ...
+openscad -q -o ./anim/${scad_file}.png -D "form=\"${form}\"" -D "cone_rotation=false" -D "animation_rotation=true" --animate ${nb_image} --imgsize ${img_size} ./${scad_file}.scad
+
+echo generating animation fixed ${form} ...
+convert -delay ${delay} -loop 0 ./anim/${scad_file}*.png ./${scad_file}.gif
+
+echo cleanup animation fixed ${form} ...
+rm -Rf ./anim/
+mv ./${scad_file}.gif ./${scad_file}_fixed_cone.gif

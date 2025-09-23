df5b801750b44d9edb2137ec41dfa18050a9b026
Yannick Battail <yannick.battail@gmail.com>
2025-09-23 19:05:16 +0200

    feat(mug): add tikawahukwa

--- mug/mugImage/generate_all.sh
+++ mug/mugImage/generate_all.sh
@@ -9,6 +9,14 @@ outFormats=png,webp,binstl
 # it takes a loooong time (and cpu, and memory) to generate
 # so it is better to generate only the model (ParameterSet) you want
 onlyParamSet=''
-#onlyParamSet='-p savoie'
+onlyParamSet='-p tikawahukwa'
+
+parallelJobs=2
+if command -v nproc >/dev/null 2>&1; then # check if the command nproc exists
+  parallelJobs=$(nproc --ignore=2)
+fi
+if ! [[ "$parallelJobs" =~ ^[1-9][0-9]*$ ]]; then # Validate that parallelJobs is a positive integer
+  parallelJobs=2
+fi
 
 npx openscad-generate@latest generate --outFormats ${outFormats} --mosaicFormat 4,4 --parallelJobs ${parallelJobs} --configFile ../globalConfig.yaml $onlyParamSet ./mugImage.scad
